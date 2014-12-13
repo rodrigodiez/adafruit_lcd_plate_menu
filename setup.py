18b5de380f98cc31db6d763487757e7267193718
Rodrigo Díez Villamuera <rodrigo@rodrigodiez.io>
2014-12-13 22:54:59 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='adafruit_lcd_plate_menu',
       version='0.1',
-      description='A simple yet powerful menu library for Adafruit\'s LCD plates'
+      description='A simple yet powerful menu library for Adafruit\'s LCD plates',
       author='Rodrigo Diez Villamuera',
       author_email='rodrigo@rodrigodiez.io',
       url='https://www.github.com/rodrigodiez/adafruit_lcd_plate_menu',
